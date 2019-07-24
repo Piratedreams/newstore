6ea715cebda0232df1ad872e589573b10ded8787
Ben Wall <wall.benjamina@gmail.com>
2019-07-24 13:00:38 -0600

    changes from yesterday

---
+++ shop/app/javascript/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    render () {
+
+        
+        
+        
+        
+        
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default App
